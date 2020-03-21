90f9c8420c77489d0fa686d14ea7e3dff2f79546
thomann mathis <tmathis@localhost.localdomain>
2020-03-21 17:25:34 +0100

    timer area

--- services/timer.js
+++ services/timer.js
@@ -1,21 +1,36 @@
 const userService = require('../users/user.service')
 const tokenService = require('../tokens/token.service')
 const spotify = require('./spotify/spotify.service')
+const areaService = require('../area/area.service')
 
-function react_to_spot(bool, who, user) {
-    if (bool)
-        console.log(user.username+ ": "+ who + ': oui');
-    else
-        console.log(user.username+ ": "+ who + ': non');
+var actArray = {"play": spotify.play,
+                "pause": spotify.pause,
+                "next": spotify.skipnext};
+
+function react_to(bool, who, user) {
+    areaService.getByTrigger(user._id, who).then(array=>{
+        array.forEach(Element =>{
+            if (typeof(Element.prevState) === 'undefined')
+                Element.prevState = false;
+            if (bool && !Element.prevState) {
+                actArray[Element.actionName](user._id);
+                Element.prevState = true;
+            } else if (!bool && Element.prevState)
+                Element.prevState = false;
+            else
+                return;
+            areaService.update(Element);
+        });
+    }).catch(error => console.error(error));
 }
 function checks_spotify(user){
     tokenService.getByType(user._id, 'spotify')
     .then(token => {
         if(!token)
             return;
-        spotify.is_play(token.value, react_to_spot, user);
-        spotify.is_device(token.value, react_to_spot, user);
-        spotify.is_shuffle(token.value, react_to_spot, user);
+        spotify.is_play(token.value, react_to, user);
+        spotify.is_device(token.value, react_to, user);
+        spotify.is_shuffle(token.value, react_to, user);
     }).catch(error => console.error(error));
 }
 
