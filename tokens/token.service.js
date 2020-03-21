2aa8e81e9c9a160929aef8aa100cf24ef0d1c887
Jeremy <jeremy@Jeremy.Ubuntu>
2020-03-21 15:27:57 +0100

    h

--- tokens/token.service.js
+++ tokens/token.service.js
@@ -28,7 +28,7 @@ async function getByType(userId, type) {
 async function create(tokenParams) {
   //const { userId, type, ...restTokenParams } = tokenParams.toObject()
   try {
-    if (await Token.findOne({userId: tokenParams.userId, type: tokenParams.type})) {
+    if (await Token.findOne({userId: tokenParams.userId, type: tokenParams.type, value: tokenParams.value})) {
         throw 'User already has a token of type: "' + tokenParams.type + '" stored'
     }
     const token = new Token(tokenParams)
@@ -40,15 +40,34 @@ async function create(tokenParams) {
   }
 }
 
-async function update(tokenId, newTokenParams) {
-  const token = await Token.findOne(tokenId)
+async function update(tokenParams) {
+  try {
+    const token = await Token.findOne({userId: tokenParams.userId, type: tokenParams.type, value: tokenParams.value})
+    if (token) {
+      Object.assign(token, tokenParams)
+      await token.save()
+      console.log('updated token')
+      return token
+    }
+    else {
+      const newToken = new Token(tokenParams)
+      await newToken.save()
+      console.log('created token')
+      return newToken
+    }
+  } catch (error) {
+    throw error
+  }
+
+
+  /*const token = await Token.findOne(tokenId)
 
   if (!token) {
     throw "token not found"
   }
 
   Object.assign(token, newTokenParams)
-  await token.save()
+  await token.save()*/
 }
 
 async function _delete(tokenId) {
